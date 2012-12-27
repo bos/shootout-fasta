41ee8d54508d3af6eca6a5929c6edf3b6d4bac47
Bryan O'Sullivan <bos@serpentine.com>
2012-12-26 20:24:32 -0800

    Switch to LUT-driven Haskell code for huge win

--- Fasta.hs
+++ Fasta.hs
@@ -3,102 +3,78 @@
 
     http://shootout.alioth.debian.org/
 
-    contributed by Branimir Maksimovic
+    contributed by Bryan O'Sullivan
 -}
 
 module Main (main) where
 
-import System.Environment (getArgs)
-import qualified Data.ByteString.Unsafe as B
-import qualified Data.ByteString.Internal as B
-import qualified Data.ByteString as B
-import Control.Monad (when, void)
-import Data.Array.Base
-import Data.Word (Word8)
-
-import Foreign.Ptr
-import Foreign.Storable (peek, poke)
+import Control.Monad
 import Foreign.C.Types
-
-type C = (UArray Int Word8,UArray Int Float)
-
-foreign import ccall unsafe "stdio.h"
-     puts  :: Ptr a -> IO ()
+import Foreign.Marshal.Alloc
+import Foreign.Ptr
+import Foreign.Storable
+import System.Environment
+import System.IO
+import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.ByteString.Unsafe as B
 
 main :: IO ()
 main = do
     n <- getArgs >>= readIO.head
 
-    B.unsafeUseAsCString alu $ \ptr -> void $
-      make ">ONE Homo sapiens alu" (n*2) (Main.repeat ptr (B.length alu)) 0
-    make ">TWO IUB ambiguity codes" (n*3) (genRandom iub) 42 >>=
-      void . make ">THREE Homo sapiens frequency" (n*5) (genRandom homosapiens)
+    writeAlu ">ONE Homo sapiens alu" (L.take (fromIntegral n*2) (L.cycle alu))
+    make ">TWO IUB ambiguity codes\n" (n*3) iub 42 >>=
+      void . make ">THREE Homo sapiens frequency\n" (n*5) homosapiens
 
-make :: B.ByteString -> Int -> (Int -> W) -> Int -> IO Int
-{-# INLINE make #-}
-make name n f seed0 = do
-    B.unsafeUseAsCString name puts
-    let line = B.replicate 61 0
-    B.unsafeUseAsCString line $ \ptr -> do
-      let make' !n !i !seed
-	      | n > 0 = do
-		  let W newseed c = f seed
-		  poke (ptr `plusPtr` i) c
-		  if i+1 >= 60
-		      then puts ptr >> make' (n-1) 0 newseed
-		      else make' (n-1) (i+1) newseed
-	      | otherwise = do
-		  when (i > 0) $ poke (ptr `plusPtr` i) (0::CChar) >> puts ptr
-		  return seed
-      make' n 0 seed0
+writeAlu :: B.ByteString -> L.ByteString -> IO ()
+writeAlu name s0 = B.putStrLn name >> go s0 >> hFlush stdout
+ where go s
+         | L.null t = L.putStrLn h >> return ()
+         | otherwise = L.putStrLn h >> go t
+         where (h,t) = L.splitAt 60 s
 
-repeat :: Ptr CChar -> Int -> Int -> W
-repeat xs n i = B.inlinePerformIO $ W i' `fmap` peek (xs `plusPtr` i)
-    where i' | i1 >= n   = 0
-             | otherwise = i1
-          i1 = i + 1
+make :: B.ByteString -> Int -> [(Char, Float)] -> Int -> IO Int
+make name n0 tbl seed0 = do
+  let modulus = 139968
+  B.unsafeUseAsCString name $ \ptr -> puts ptr (B.length name - 1)
+  let convert = scanl1 (\(_,p) (c,q) -> (c,p+q))
+      fill ((c,p):cps) !j =
+	let !k = min modulus (floor (fromIntegral modulus * p + 1))
+	in B.replicate (fromIntegral (k - j)) c : fill cps k
+      fill _ _ = []
+  let lookupTable = B.concat $ fill (convert tbl) 0
+  allocaBytes 61 $ \ptr -> do
+    let make' !n !i !seed
+	    | n > 0 = do
+		let newseed = (seed * 3877 + 29573) `rem` modulus
+		poke (ptr `plusPtr` i) (lookupTable `B.unsafeIndex` newseed)
+		if i+1 >= 60
+		    then puts ptr 60 >> make' (n-1) 0 newseed
+		    else make' (n-1) (i+1) newseed
+	    | otherwise = do
+		when (i > 0) $ poke (ptr `plusPtr` i) (0::CChar) >> puts ptr i
+		return seed
+    make' n0 0 seed0
 
-data W = W {-# UNPACK #-} !Int {-# UNPACK #-} !Word8
+alu :: L.ByteString
+alu = "GGCCGGGCGCGGTGGCTCACGCCTGTAATCCCAGCACTTTGGGAGGCCGAGGCGGGCGGATCACCTGAGG\
+    \TCAGGAGTTCGAGACCAGCCTGGCCAACATGGTGAAACCCCGTCTCTACTAAAAATACAAAAATTAGCCGGG\
+    \CGTGGTGGCGCGCGCCTGTAATCCCAGCTACTCGGGAGGCTGAGGCAGGAGAATCGCTTGAACCCGGGAGGC\
+    \GGAGGTTGCAGTGAGCCGAGATCGCGCCACTGCACTCCAGCCTGGGCGACAGAGCGAGACTCCGTCTCAAAAA"
 
-genRandom :: C -> Int -> W
-genRandom (!a,!b) seed = find 0
-  where find i
-            | b `unsafeAt` i >= rnd = W newseed (a `unsafeAt` i)
-            | otherwise = find (i+1)
-        D newseed rnd = genRand seed
+iub, homosapiens :: [(Char, Float)]
 
-data D = D {-# UNPACK #-} !Int {-# UNPACK #-} !Float
+iub = [('a',0.27),('c',0.12),('g',0.12),('t',0.27),('B',0.02)
+      ,('D',0.02),('H',0.02),('K',0.02),('M',0.02),('N',0.02)
+      ,('R',0.02),('S',0.02),('V',0.02),('W',0.02),('Y',0.02)]
 
-genRand :: Int -> D
-genRand seed = D newseed newran
-  where
-    newseed = (seed * ia + ic) `rem` im
-    newran  =  fromIntegral newseed * rimd
-    rimd      = 1.0 / fromIntegral im
-    im, ia, ic :: Int
-    im  = 139968
-    ia  = 3877
-    ic  = 29573
+homosapiens = [('a',0.3029549426680),('c',0.1979883004921)
+              ,('g',0.1975473066391),('t',0.3015094502008)]
 
-alu :: B.ByteString
-alu =
-    "GGCCGGGCGCGGTGGCTCACGCCTGTAATCCCAGCACTTTGG\
-    \GAGGCCGAGGCGGGCGGATCACCTGAGGTCAGGAGTTCGAGA\
-    \CCAGCCTGGCCAACATGGTGAAACCCCGTCTCTACTAAAAAT\
-    \ACAAAAATTAGCCGGGCGTGGTGGCGCGCGCCTGTAATCCCA\
-    \GCTACTCGGGAGGCTGAGGCAGGAGAATCGCTTGAACCCGGG\
-    \AGGCGGAGGTTGCAGTGAGCCGAGATCGCGCCACTGCACTCC\
-    \AGCCTGGGCGACAGAGCGAGACTCCGTCTCAAAAA"
+puts :: Ptr a -> Int -> IO ()
+puts p n = do
+  poke (p `plusPtr` n) (10 :: CChar)
+  void $ write 1 p (fromIntegral n + 1)
 
-mkCum :: [(Char,Float)] -> C
-mkCum lst = (listArray (0, length lst - 1) . map (B.c2w . fst) $ lst,
-      	     listArray (0, length lst - 1) . scanl1 (+) . map snd $ lst)
-
-homosapiens, iub :: C
-
-iub = mkCum [('a',0.27),('c',0.12),('g',0.12),('t',0.27),('B',0.02)
-        ,('D',0.02),('H',0.02),('K',0.02),('M',0.02),('N',0.02)
-        ,('R',0.02),('S',0.02),('V',0.02),('W',0.02),('Y',0.02)]
-
-homosapiens = mkCum [('a',0.3029549426680),('c',0.1979883004921)
-                ,('g',0.1975473066391),('t',0.3015094502008)]
+foreign import ccall unsafe write :: CInt -> Ptr a -> CSize -> IO CSize
